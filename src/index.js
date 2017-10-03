4a821cbd329a6d01f28cc7898b10f6997fd46c47
Aissam Salhi <ais.salhy@gmail.com>
2017-10-03 22:58:04 +0200

    Added Redux COnfiguration, now ready

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import {createStore, applyMiddleware} from 'redux';
+import {Provider} from 'react-redux'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+import './styles/index.css';
+import App from './components/App';
+import registerServiceWorker from './registerServiceWorker';
+import reducers from './reducers';
+
+
+
+const createStoreWithMiddleWare= applyMiddleware()(createStore);
+
+
+ReactDOM.render(
+    <Provider store={createStoreWithMiddleWare(reducers)} >
+        <App />
+    </Provider>
+, document.getElementById('root'));
 registerServiceWorker();
